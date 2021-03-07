8643660e53ad07ee766c0f69327add7a2f54c045
Koen Van Geert <koen.van.geert@gmail.com>
2021-03-07 11:32:06 +0100

    Drag part 2

--- src/Page.tsx
+++ src/Page.tsx
@@ -1,10 +1,11 @@
-import { motion, MotionStyle, MotionValue } from "framer-motion";
+import { motion, MotionStyle, MotionValue, PanInfo } from "framer-motion";
 import React, { FunctionComponent } from "react";
 
 interface PageProps {
   index: number;
   renderPage: (props: { index: number }) => JSX.Element;
   x: MotionValue;
+  onDragEnd(event: MouseEvent | TouchEvent | PointerEvent, info: PanInfo): void;
 }
 
 const pageStyle: MotionStyle = {
@@ -17,6 +18,7 @@ export const Page: FunctionComponent<PageProps> = ({
   index,
   renderPage,
   x,
+  onDragEnd,
 }) => {
   const child = React.useMemo(() => renderPage({ index }), [index, renderPage]);
 
@@ -28,8 +30,10 @@ export const Page: FunctionComponent<PageProps> = ({
         left: `${index * 100}%`,
         right: `${index * 100}%`,
       }}
+      draggable
       drag="x"
       dragElastic={1}
+      onDragEnd={onDragEnd}
     >
       {child}
     </motion.div>
--- src/VirtualizedPage.tsx
+++ src/VirtualizedPage.tsx
@@ -1,5 +1,12 @@
 import * as React from "react";
-import { motion, MotionStyle, useMotionValue } from "framer-motion";
+import {
+  animate,
+  AnimationOptions,
+  motion,
+  MotionStyle,
+  PanInfo,
+  useMotionValue,
+} from "framer-motion";
 import { Page } from "./Page";
 
 const range = [-1, 0, 1];
@@ -15,19 +22,52 @@ const containerStyle: MotionStyle = {
   overflowX: "hidden",
 };
 
+const transition: AnimationOptions<any> = {
+  type: "spring",
+  bounce: 0,
+};
+
 export const VirtualizedPage: React.FunctionComponent<VirtualizedPageProps> = ({
   children,
 }) => {
   const x = useMotionValue(0);
+  const containerRef = React.useRef<HTMLDivElement>(null);
   const [index, setIndex] = React.useState(0);
 
+  const calculateNewX = () => -index * (containerRef.current?.clientWidth || 0);
+
+  const handleEndDrag = (e: Event, dragProps: PanInfo) => {
+    const clientWidth = containerRef.current?.clientWidth || 0;
+
+    const { offset, velocity } = dragProps;
+
+    if (Math.abs(velocity.y) > Math.abs(velocity.x)) {
+      animate(x, calculateNewX(), transition);
+      return;
+    }
+
+    if (offset.x > clientWidth / 4) {
+      setIndex(index - 1);
+    } else if (offset.x < -clientWidth / 4) {
+      setIndex(index + 1);
+    } else {
+      animate(x, calculateNewX(), transition);
+    }
+  };
+
+  React.useEffect(() => {
+    const controls = animate(x, calculateNewX(), transition);
+    return controls.stop;
+  }, [index]);
+
   return (
-    <motion.div style={containerStyle}>
+    <motion.div ref={containerRef} style={containerStyle}>
       {range.map((rangeValue) => {
         return (
           <Page
             key={rangeValue + index}
             x={x}
+            onDragEnd={handleEndDrag}
             index={rangeValue + index}
             renderPage={children}
           />
